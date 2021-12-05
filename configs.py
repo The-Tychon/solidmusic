d2f5314ed8e14d27985378d330121f154c158dfe
The-Tychon <93827022+The-Tychon@users.noreply.github.com>
2021-12-06 00:26:35 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,9 +17,9 @@ class Configs:
     BOT_TOKEN = getenv("BOT_TOKEN", "123:abc")
     OWNER_ID = int(getenv("OWNER_ID", "0123"))
     SESSION = getenv("SESSION", "session")
-    CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/solidprojects")
-    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/solidprojects_chat")
-    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DoellBarr/solidmusic")
+    CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/electro_updates")
+    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/electrobot_support")
+    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/itz_me_tychon")
     AUTO_LEAVE = int(getenv("AUTO_LEAVE", "1800"))
 
 
